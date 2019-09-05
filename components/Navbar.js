1295e4ff23f237f301fcb3195404454dd787a6d6
jacksorrells <jrs08d@acu.edu>
2019-09-04 21:20:40 -0500

    2 files modified, 1 added made some changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, Icon } from 'antd';
 
+import { navData } from '../data/navData';
+
 const { SubMenu } = Menu;
 
 
 function Navbar() {
-  const [page, setPage] = useState({ current: [] });
+  const [page, setPage] = useState("cardView");
 
   const handleClick = (e) => {
     console.log('click', e);
@@ -13,32 +15,14 @@ function Navbar() {
   };
 
   return (
-    <Menu onClick={handleClick} selectedKeys={[page.current]} mode="horizontal">
-      <Menu.Item key="home">
-        <Icon type="mail" />
-        Home
-      </Menu.Item>
-      <Menu.Item key="app" disabled>
-        <Icon type="appstore" />
-        Navigation Two
-      </Menu.Item>
-      <SubMenu
-        title={
-          <span className="submenu-title-wrapper">
-            <Icon type="setting" />
-            Navigation Three - Submenu
-          </span>
-        }
-      >
-        <Menu.ItemGroup title="Item 1">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
-        </Menu.ItemGroup>
-        <Menu.ItemGroup title="Item 2">
-          <Menu.Item key="setting:3">Option 3</Menu.Item>
-          <Menu.Item key="setting:4">Option 4</Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu>
+    <Menu onClick={handleClick} selectedKeys={[page]} mode="horizontal">
+      {navData.map(navItem => {
+          return (
+            <Menu.Item key={navItem.name}>
+              <Icon type={navItem.icon} /> {navItem.displayName}
+            </Menu.Item>
+          );
+        })}
     </Menu>
   )
 }
---
+++ data/navData.js
@@ -0,0 +1,14 @@
+export const navData = [
+  {
+    name: "cardView",
+    displayName: "Card View",
+    icon: "appstore",
+    href: "/card"
+  },
+  {
+    name: "listView",
+    displayName: "List View",
+    icon: "unordered-list",
+    href: "/list"
+  }
+];
