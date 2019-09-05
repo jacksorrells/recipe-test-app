a764f021ac3dca316c08d550c1fe8bb886eb99aa
jacksorrells <jrs08d@acu.edu>
2019-09-04 22:08:09 -0500

    1 file modified, 2 added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from './components/Navbar';
 import CardContainer from './components/CardContainer';
