6e140d58283c173347cb16aef25eb7cabe5c1038
jacksorrells <jrs08d@acu.edu>
2019-09-15 21:01:22 -0500

    1 file modified

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -11,68 +11,77 @@ function Recipe() {
   return (
     <Card>
       <Typography>
-        <Title level={1}>{recipeData[0].name}</Title>
+        <Title level={1}>
+          <a href={recipeData[0].url} target="_blank">
+            {recipeData[0].name}
+          </a>
+        </Title>
 
         <Paragraph>
           {recipeData[0].description}
         </Paragraph>
 
         <Row>
-          <Col span="12">
+          <Col span={12}>
 
-        
-        <Paragraph>
-          <Text strong>Link: </Text> {recipeData[0].link}
-        </Paragraph>
-        <Paragraph>
-          <Text strong>Course: </Text> {recipeData[0].course}
-        </Paragraph>
-        <Paragraph>
-          <Text strong>Cuisine: </Text> {recipeData[0].cuisine}
-        </Paragraph>
-        <Paragraph>
-          <Text strong>Servings: </Text> {recipeData[0].servings}
-        </Paragraph>
+            <Paragraph>
+              <Text strong>Course: </Text> {recipeData[0].course}
+            </Paragraph>
+            <Paragraph>
+              <Text strong>Cuisine: </Text> {recipeData[0].cuisine}
+            </Paragraph>
+            <Paragraph>
+              <Text strong>Servings: </Text> {recipeData[0].servings}
+            </Paragraph>
 
           </Col>
 
           <Col span={12}>
 
-        <Paragraph>
-          <Text strong>Prep Time: </Text> {recipeData[0].prepTime}
-        </Paragraph>
-        <Paragraph>
-          <Text strong>Cook Time: </Text> {recipeData[0].cookTime}
-        </Paragraph>
-        <Paragraph>
-          <Text strong>Total Time: </Text> {recipeData[0].totalTime}
-        </Paragraph>
+            <Paragraph>
+              <Text strong>Prep Time: </Text> {recipeData[0].prepTime}
+            </Paragraph>
+            <Paragraph>
+              <Text strong>Cook Time: </Text> {recipeData[0].cookTime}
+            </Paragraph>
+            <Paragraph>
+              <Text strong>Total Time: </Text> {recipeData[0].totalTime}
+            </Paragraph>
         
           </Col>
         </Row>
 
         <Divider />
 
-        <Title level={3}>Ingredients:</Title>
-        <ul>
-          {recipeData[0].ingredients.map(ingredient => {
-            return (
-              <li>{ingredient}</li>
-            );
-          })}
-        </ul>
+        <Row>
+          <Col>
+            <Title level={3}>Ingredients:</Title>
+            <ul>
+              {recipeData[0].ingredients.map(ingredient => {
+                return (
+                  <li>{ingredient}</li>
+                );
+              })}
+            </ul>
+          </Col>
+        </Row>
 
         <Divider />
 
-        <Title level={3}>Instructions:</Title>
-        <ol>
-          {recipeData[0].instructions.map(instruction => {
-            return (
-              <li>{instruction}</li>
-            );
-          })}
-        </ol>
+        <Row>
+          <Col>
 
+            <Title level={3}>Instructions:</Title>
+            <ol>
+              {recipeData[0].instructions.map(instruction => {
+                return (
+                  <li>{instruction}</li>
+                );
+              })}
+            </ol>
+
+          </Col>
+        </Row>
 
       </Typography>
     </Card>
